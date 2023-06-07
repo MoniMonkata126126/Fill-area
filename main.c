51dfeeb23feac5e3304277cd9c1835e242b61ead
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2023-06-07 22:04:11 +0300

    Base logic outline and function params

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct pixels{
+struct pixel{
     unsigned char R;
     unsigned char G;
     unsigned char B;
 };
 
-int main(int argc, char** argv){
-    FILE* fp;
-    fp = fopen(argv[1], "r");
-    if(fp == NULL){
-        printf("Error!");
-        exit(1);
-    }
-    fclose(fp);
+
+struct pixel** fill_area(struct pixel** img, int y_coord, int x_coord){
+    //queue e reshenieto
+}
+
+
+int main(){
+
+    int matrix[4][12] = {{1, 2, 3, 234, 243, 255, 2, 35, 64, 255, 255, 255},
+                {1, 2, 3, 234, 243, 255, 2, 35, 64, 255, 255, 255},
+                {1, 2, 3, 234, 243, 255, 2, 35, 64, 255, 255, 255},
+                {1, 2, 3, 234, 243, 255, 2, 35, 64, 255, 255, 255}};
+
     return 0;
 }
